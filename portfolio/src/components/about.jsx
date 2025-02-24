2c7ee9d4e2a84e53d0c125da0cc6067c4cf294ff
Sivasanjay24 <sanjaysanjith2004@gmail.com>
2025-02-24 08:28:35 +0530

    c1

--- portfolio/src/components/about.jsx
+++ portfolio/src/components/about.jsx
@@ -5,7 +5,7 @@ function about()
     <div className="about">
       <h1 className="ha">About Me</h1>
       <center>
-      <img className="imga"src='/me.jpg'></img>
+      <img className="imga"src='/me.png'></img>
       <p className='ac'>I am a diligent and enthusiastic 3rd-year Computer Science and Engineering student with intermediate proficiency in core CS subjects such as DBMS, DSA, CN, and OS, complemented by practical experience in web technologies including HTML, CSS, JavaScript, and the MERN stack. My passion for coding motivates me to continuously explore and adopt new tools and technologies, such as Git and VS Code, to develop efficient and innovative solutions. I take pride in solving complex problems and transforming ideas into functional applications, with a strong emphasis on writing clean, scalable, and maintainable code. Beyond my academic pursuits, I actively participate in personal projects and collaborative coding initiatives to enhance my skills and stay abreast of industry advancements. In my leisure time, I enjoy playing games and reading books, activities that foster creativity and strategic thinking while providing a balanced perspective. I am eager to further develop my expertise and contribute meaningfully to impactful projects in the field of technology.</p>
       </center>
       </div>
