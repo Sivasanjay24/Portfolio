91098c08c1995d03b4a410f51117b06ca68a8eb2
Sivasanjay24 <sanjaysanjith2004@gmail.com>
2025-02-25 09:49:33 +0530

    'c3'

--- portfolio/src/components/home.jsx
+++ portfolio/src/components/home.jsx
@@ -4,7 +4,10 @@ function home()
   return (
     <div className="home">
       <div className='con'>
-      <h1 className="h">Sivasanjay S</h1>
+      <div className="content">
+      <span className="h"> Hi,I'm <span className='nm'>Sivasanjay</span></span><br></br>
+      <span className="h4">Computer Science and Engineering Undergrad</span>
+      </div>
       <img className="imga"src='/me.png'></img>
       </div >
     </div>
