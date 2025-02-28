d6c0b6cf269e250c8cfba9e571e96627b6ad36c5
Sivasanjay24 <sanjaysanjith2004@gmail.com>
2025-02-28 13:52:16 +0530

    Glitch eff

--- portfolio/src/components/home.jsx
+++ portfolio/src/components/home.jsx
@@ -1,11 +1,16 @@
 import './css/home.css'
+import Glitch from './glitch'
 function home()
 {
   return (
     <div className="home">
       <div className='con'>
       <div className="content">
-      <span className="h"> Hi,I'm <span className='nm'>Sivasanjay</span></span><br></br>
+      <span className="h"> Hi,I'm <Glitch
+   speed={0.6}
+   enableShadows={true}
+   enableOnHover={true}
+   className='custom-class'>Sivasanjay</Glitch></span>
       <span className="h4">Computer Science and Engineering Undergrad</span>
       </div>
       <img className="imga"src='/me.png'></img>
